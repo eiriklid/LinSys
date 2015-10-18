56b8870cec3543595fe7a53e3ce54829ae9ee47a
solveds <s_dahlin7@hotmail.com>
2015-10-18 19:21:12 +0200

    Create pres_init_heli.m

---
+++ pres_init_heli.m
@@ -0,0 +1,23 @@
+% Initialization for the helicopter assignment in the course TTK4115.
+% Run this file before you execute QuaRC_ -> Build to build the file heli_q4.mdl.
+
+% Oppdatert h�sten 2006 av Jostein Bakkeheim
+% Oppdatert h�sten 2008 av Arnfinn Aas Eielsen
+% Oppdatert h�sten 2009 av Jonathan Ronen
+% Updated fall 2010, Dominik Breu
+% Updated fall 2013, Mark Haring
+% Updated spring 2015, Mark Haring
+
+
+%%%%%%%%%%% Calibration of the encoder and the hardware for the specific helicopter
+Joystick_gain_x = -1;
+Joystick_gain_y = 1;
+
+
+%%%%%%%%%%% Physical constants
+g = 9.81; % gravitational constant [m/s^2]
+l_c = 0.46; % distance elevation axis to counterweight [m]
+l_h = 0.66; % distance elevation axis to helicopter head [m]
+l_p = 0.175; % distance pitch axis to motor [m]
+m_c = 1.92; % Counterweight mass [kg]
+m_p = 0.72; % Motor mass [kg]
